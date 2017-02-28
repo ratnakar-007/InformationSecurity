a736bdc90aea2351f325eedbae7c9bf6b95fdece
ratnakar-007 <ratnakarsharma.007@gmail.com>
2017-02-28 11:44:06 +0530

    Permutation

---
+++ NbitsStrings.java
@@ -0,0 +1,24 @@
+public class NbitsStrings {
+	int[] arrA;
+
+	public NbitsStrings(int n) {
+		arrA = new int[n];
+	}
+
+	public void nBits(int n) {
+		if (n <= 0) {
+			System.out.println(Arrays.toString(arrA));
+		} else {
+			arrA[n - 1] = 0;
+			nBits(n - 1);
+			arrA[n - 1] = 1;
+			nBits(n - 1);
+		}
+	}
+
+	public static void main(String[] args) throws java.lang.Exception {
+		int n = 3;
+		NbitsStrings i = new NbitsStrings(n);
+		i.nBits(n);
+	}
+}
